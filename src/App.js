5e6d98364491dbb53847f19c6c2d64f4f17d1d93
Dimitris Damilos <d.k.damilos@gmail.com>
2018-11-11 02:21:35 +0000

    :fire: Remove unused files

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import Button from '@material-ui/core/Button';
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="app-titles">
-          <Typography component="h1" variant="h3" className="white logo" align="center">
-            Costie
+      <div className="app-titles">
+        <Typography component="h1" variant="h3" className="white logo" align="center">
+          Costie
+        </Typography>
+        <Typography component="h2" variant="h5" className="white" align="center">
+          Time is money.
+        </Typography>
+        <Card className="centered-card form">
+          <Button variant="fab" aria-label="Currency" className="currency">
+            &pound;
+          </Button>
+          <Typography color="textSecondary" gutterBottom>
+            Replace me with the form
           </Typography>
-          <Typography component="h2" variant="h5" className="white" align="center">
-            Time is money.
-          </Typography>
-          <Card className="centered-card form">
-            <Button variant="fab" aria-label="Currency" className="currency">
-              &pound;
-            </Button>
-            <Typography color="textSecondary" gutterBottom>
-              Replace me with the form
-            </Typography>
-          </Card>
-        </div >
-      </React.Fragment>
+        </Card>
+      </div>
     );
   }
 }
