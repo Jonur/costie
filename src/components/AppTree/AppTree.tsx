a96a0f1a37c0a4cdab756ba00996034b6aebc022
Jonur <d.k.damilos@gmail.com>
2026-02-24 18:32:48 +0000

    Add main tree

--- src/components/AppTree/AppTree.tsx
+++ src/components/AppTree/AppTree.tsx
@@ -4,12 +4,12 @@ import Header from "../Header";
 import LanguageSelector from "../LanguageSelector";
 
 const AppTree = () => (
-  <div className="w-3/4 max-w-75 mt-20 h-md:mt-50">
+  <main className="w-3/4 max-w-75 mt-20 h-md:mt-50">
     <LanguageSelector />
     <Header />
     <CostieFormFlow />
     <Footer />
-  </div>
+  </main>
 );
 
 export default AppTree;
