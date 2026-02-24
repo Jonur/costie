c27a35622990423372dcd3d9b377ccb575b3e102
Jonur <d.k.damilos@gmail.com>
2026-02-24 20:56:00 +0000

    Attempt preloading fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
+import bubblegumWoff2 from "@fontsource/bubblegum-sans/files/bubblegum-sans-latin-400-normal.woff2?url";
+import robotoWoff2 from "@fontsource/roboto/files/roboto-latin-400-normal.woff2?url";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/bubblegum-sans/400.css";
 import "./styles.css";
 
 import AppTree from "./components/AppTree";
 import { CostieProvider } from "./state";
+
+const preloadFont = (href: string) => {
+  const link = document.createElement("link");
+  link.rel = "preload";
+  link.as = "font";
+  link.type = "font/woff2";
+  link.href = href;
+  link.crossOrigin = "anonymous";
+  document.head.appendChild(link);
+};
+
+preloadFont(robotoWoff2);
+preloadFont(bubblegumWoff2);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
