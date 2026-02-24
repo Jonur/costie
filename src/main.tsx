c9788bbd9018a32af7462c950ff819f5f6e517f5
Jonur <d.k.damilos@gmail.com>
2026-02-24 18:40:10 +0000

    Remove network calls

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/bubblegum-sans/400.css";
 import "./styles.css";
+
 import AppTree from "./components/AppTree";
 import { CostieProvider } from "./state";
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: "esbuild",
+    sourcemap: false,
+  },
   plugins: [react(), tailwindcss()],
 });
